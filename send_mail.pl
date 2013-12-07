82f93f1a80e956bfdfc2b399fdba5c212171d91c
char6ming <caoliuming@163.com>
2013-12-07 21:51:43 +0800

    Create send_mail.pl
    I use this script send mail

---
+++ send_mail.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Mail::Sendmail;
+
+2 == 0 + @ARGV or die "pls feed 2 args, 1st is subject, 2nd is content file";
+
+my %mail = (
+	server	=> 'smtp.xxxxxxx.com:25',
+	auth    => {user => 'char6ming@xxxxxxx.com', password => '', method => 'LOGIN', required => 1},
+	From	=> 'caolm@xxxxxxx.com',
+);
+
+$mail{To} = 'someone@xxxxxxx.com,caolm@xxxxxxx.com';
+$mail{Cc} = 'someone@xxxxxxx.com,caolm@xxxxxxx.com';
+
+$mail{Subject} = shift @ARGV;
+
+{
+	local $/ = undef;
+	$mail{Message} = <>;
+}
+
+sendmail (%mail) or die $Mail::Sendmail::error;
+print "OK. Log says:\n", $Mail::Sendmail::log;
+
+__END__
